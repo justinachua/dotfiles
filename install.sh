5a62e384f81171a008bb8ef2c52233917bb7ad5c
Justina <justina.chua@shopify.com>
2022-05-17 18:09:42 -0400

    Comment out git clone in install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
 fi
 
 # auto complete for zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions \
-  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions \
+#   ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting \
-  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+#   ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 
 # Link dotfiles
